63768d562bd99f44f38964a63877912cf9d1ef0b
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-07-10 16:10:28 +0530

    created controller to get rider by id

--- apps/riders-service/src/riders-service.controller.ts
+++ apps/riders-service/src/riders-service.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { RidersServiceService } from './riders-service.service';
+import { MessagePattern } from '@nestjs/microservices';
 
-@Controller()
+@Controller('rider')
 export class RidersServiceController {
   constructor(private readonly ridersServiceService: RidersServiceService) {}
 
-  @Get()
-  getHello(): string {
-    return this.ridersServiceService.getHello();
+  // @Get(':id')
+  @MessagePattern({ cmd: 'get-rider' })
+  async getRiderById(data: any) {
+    return Promise.resolve({
+      _id: data.id,
+      firstName: 'Lahiru',
+      lastName: 'Karunathilaka',
+      email: 'lahiru@example.com',
+    });
   }
 }
